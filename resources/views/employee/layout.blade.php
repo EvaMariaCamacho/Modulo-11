d565d97818fbbd93e94ad93cabb04880ef32639f
Eva Maria <evamariacamacho@gmail.com>
2021-05-27 15:51:38 +0200

    commit pagina home

--- resources/views/employee/layout.blade.php
+++ resources/views/employee/layout.blade.php
@@ -1,9 +1,10 @@
 @extends('layouts.app')
 
 @section('content')
-<h1>HOME</h1>
+
 <div class="container">
-    <div class="row justify-content-center">
+    <h1>HOME</h1>
+   <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">{{ __('Dashboard') }}</div>
